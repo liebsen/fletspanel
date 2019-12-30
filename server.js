f4e34d893add624b3d278d95e9f36df792e830fd
martin <telemgico@gmail.com>
2019-12-29 21:38:15 -0300

    cache and compression

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 var express = require('express');
 var path = require('path');
+var compression = require('compression');
 var sslRedirect = require('heroku-ssl-redirect');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(sslRedirect());
+app.use(compression());
 // Set static files
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/dist',{
+    maxAge: "1d"
+}))
 
 app.get('*', function(req, res){
 	res.sendFile('index.html', { root: __dirname + '/dist' });
