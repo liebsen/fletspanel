b67c347e295b83d9d2f1acf911898ace1980e067
liebsen <overlemonsoft@gmail.com>
2022-09-22 20:42:17 -0300

    build

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening at http://localhost:' + server.address().port);
-});
+})
